e5d74bd535123fd373a138da54c494c30638ac6c
Manohar <42617348+ManoharsaiV@users.noreply.github.com>
2019-07-31 21:16:11 -0400

    Update LuckyDivision.java

--- LuckyDivision.java
+++ LuckyDivision.java
@@ -4,10 +4,53 @@ import java.util.Scanner;
 
 public class LuckyDivision {
 
-	public static boolean checkIfAlmostLucky(int number) {
-		
+	public static boolean checkIfLucky(int number) {
+		boolean result = false;
+		int numOfDigits = 0;
+		String num = Integer.valueOf(number).toString();
+		char[] ch = num.toCharArray();
 
-		return false;
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == '4' || ch[i] == '7') {
+				numOfDigits++;
+			} else {
+				break;
+			}
+		}
+
+		if (numOfDigits == ch.length) {
+			result = true;
+		} else {
+			result = false;
+		}
+
+		return result;
+	}
+
+	public static boolean checkIfAlmostLucky(int number) {
+		if (checkIfLucky(number)) {
+			return true;
+		}
+		boolean result = false;
+		int quotient_4 = 0, quotient_7 = 0, quotient_28 = 0, quotient_47 = 0;
+		if (number % 47 == 0) {
+			quotient_47 = number / 47;
+		} else if (number % 28 == 0) {
+			quotient_28 = number / 28;
+		} else if (number % 4 == 0) {
+			quotient_4 = number / 4;
+		} else if (number % 7 == 0) {
+			quotient_7 = number / 7;
+		}
+
+		if (!(quotient_4 == 0 && quotient_7 == 0 && quotient_28 == 0 && quotient_47 == 0)
+				&& (quotient_4 % 2 == 0 || quotient_7 % 2 == 0 || quotient_28 % 2 == 0 || quotient_47 % 2 == 0)) {
+			result = true;
+		} else {
+			result = false;
+		}
+
+		return result;
 	}
 
 	public static void main(String[] args) {
