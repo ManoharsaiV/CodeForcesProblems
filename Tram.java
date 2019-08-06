cd7cc504d6ee7baecfa91e4772d771b630f1eaa8
Manohar <42617348+ManoharsaiV@users.noreply.github.com>
2019-08-06 15:15:48 -0400

    Update Tram.java

--- Tram.java
+++ Tram.java
@@ -5,56 +5,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Tram {
-	static List<Integer> li = new ArrayList<>();
+    static int minTramCapacity = 0;
+    static int passengersInTram = 0;
 
-	public static void tramCapacity(int[] a) {
+    public static void tramCapacity(int[] a) {
+        int out = a[0], in = a[1];
+        passengersInTram = passengersInTram + in - out;
+        if (passengersInTram < minTramCapacity) {
+            minTramCapacity = minTramCapacity;
+        } else {
+            minTramCapacity = passengersInTram;
+        }
+    }
 
-		int out = a[0], in = a[1];
-		int passengersInTram = 0;
-		if (out == 0) {
-			passengersInTram += in;
-		} else {
-			passengersInTram = passengersInTram + in - out;
-		}
 
-		li.add(passengersInTram);
+    public static void main(String[] args) {
 
-	}
-	
+        Scanner scan = new Scanner(System.in);
+        int stops = Integer.parseInt(scan.nextLine());
 
-	public static int justToSort(List<Integer> l) {
-		int temp = l.get(0);
-		for (int i = 0; i < l.size(); i++) {
-			if (temp < l.get(i)) {
-				temp = l.get(i);
-			} else {
-				continue;
-			}
-		}
+        while (stops > 0) {
+            String[] totalPassangers = scan.nextLine().split(" ");
+            int index = 0;
+            int[] a = new int[totalPassangers.length];
+            while (index < a.length) {
+                a[index] = Integer.parseInt(totalPassangers[index]);
+                index++;
+            }
+            tramCapacity(a);
+            stops--;
+        }
+        System.out.println(minTramCapacity);
 
-		return temp;
-	}
+        scan.close();
 
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		int stops = Integer.parseInt(scan.nextLine());
-
-		while (stops > 0) {
-			String[] totalPassangers = scan.nextLine().split(" ");
-			int index = 0;
-			int[] a = new int[totalPassangers.length];
-			while (index < a.length) {
-				a[index] = Integer.parseInt(totalPassangers[index]);
-				index++;
-			}
-			
-
-			stops--;
-		}
-
-		scan.close();
-
-	}
+    }
 
 }
+
