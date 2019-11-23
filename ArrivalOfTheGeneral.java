ff50c925ad8aaa9c22a0b062abe094cd381f49a4
Manohar <42617348+ManoharsaiV@users.noreply.github.com>
2019-11-23 03:51:17 -0500

    Update ArrivalOfTheGeneral.java

--- ArrivalOfTheGeneral.java
+++ ArrivalOfTheGeneral.java
@@ -25,7 +25,9 @@ public class ArrivalOfTheGeneral {
             }
             index--;
         }
-
+        // when the min and max meet, the min element is swapped with the max element
+        // thereby one less place to travel for the max element. hence in that case, 
+        // maxIndex -= 1
         if (maxIndex > minIndex) {
             maxIndex -= 1;
         }
@@ -50,37 +52,3 @@ public class ArrivalOfTheGeneral {
 
 
 
-/**
- *  private static int minimumTimeRequired(int[] soldiers) {
- *             int count_1 = 0, count_2 = 0;
- *             int[] soldiers1 = new int[soldiers.length];
- *             for (int i = 0; i < soldiers1.length; i++) {
- *                 soldiers1[i] = soldiers[i];
- *             }
- *             int[] soldiers2 = new int[soldiers.length];
- *             for (int i = 0; i < soldiers2.length; i++) {
- *                 soldiers2[i] = soldiers[i];
- *             }
- *             for (int i = 0; i < soldiers1.length; i++) {
- *                 for (int j = 0; j < soldiers1.length; j++) {
- *                     if (soldiers1[i] < soldiers1[j]) {
- *                         soldiers1[i] = soldiers1[0] ^ soldiers1[i];
- *                         soldiers1[i] = soldiers1[0] ^ soldiers1[i];
- *                         soldiers1[0] = soldiers1[0] ^ soldiers1[i];
- *                         count_1++;
- *                     }
- *                 }
- *
- *             }
- *             for (int i = soldiers2.length - 1; i > 0; i--) {
- *                 if (soldiers2[soldiers2.length - 1] > soldiers2[i]) {
- *                     soldiers2[soldiers2.length - 1] = soldiers2[soldiers2.length - 1] ^ soldiers2[i];
- *                     soldiers2[i] = soldiers2[soldiers2.length - 1] ^ soldiers2[i];
- *                     soldiers2[soldiers2.length - 1] = soldiers2[soldiers2.length - 1] ^ soldiers2[i];
- *                     count_2++;
- *                 }
- *             }
- *
- *             return count_1 + count_2;
- *         }
- *  */
